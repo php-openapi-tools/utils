e83f5d031f62a47419ea2da5751aee59d59a7b14
Cees-Jan Kiewiet <ceesjank@gmail.com>
2024-11-06 12:34:41 +0100

    Provide constants for do or do not load on write

--- src/File.php
+++ src/File.php
@@ -8,6 +8,9 @@ use PhpParser\Node;
 
 final readonly class File
 {
+    public const DO_LOAD_ON_WRITE = true;
+    public const DO_NOT_LOAD_ON_WRITE = false;
+
     public function __construct(
         public string $pathPrefix,
         public string $fqcn,
